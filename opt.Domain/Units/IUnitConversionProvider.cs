6f941bf20e4208355c92507db7d45c411b91f71b
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2014-06-15 14:01:04 +0300

    Added temp comment
    It concerns the expected behavior of interface implementers. Later will
    be replaced with proper XML comment.

--- opt.Domain/Units/IUnitConversionProvider.cs
+++ opt.Domain/Units/IUnitConversionProvider.cs
@@ -3,6 +3,7 @@ namespace opt.Units
 {
     public interface IUnitConversionProvider<TValue>
     {
+        // Is expected to return null if conversion is not found, NOT throw KeyNotFound or other exception.
         UnitConversion<TValue> GetConversion(IUnit fromUnit, IUnit toUnit);
     }
 }
