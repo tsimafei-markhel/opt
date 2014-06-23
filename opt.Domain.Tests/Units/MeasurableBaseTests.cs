f58ca9c0e12a7d4a2030c31cb3cbaf9e4b3d421e
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2014-06-23 12:39:02 +0300

    MeasurableBase<TValue>: comments & unit tests upd
    - Removed TODOs from MeasurableBase<TValue>;
    - Added XML comments for MeasurableBase<TValue>;
    - Added unit tests for MeasurableBase<TValue>.

---
+++ opt.Domain.Tests/Units/MeasurableBaseTests.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using opt.Units;
+
+namespace opt.Domain.Tests.Units
+{
+    // Not testing operators and object overrides as long as they use
+    // tested IComparable<T> and IEquatable<T> implementations
+
+    [TestClass]
+    public class MeasurableBaseTests
+    {
+        class FakeMeasurable : MeasurableBase<double>
+        {
+            public FakeMeasurable(IUnit unit, double value) :
+                base(unit, value)
+            {
+            }
+
+            public FakeMeasurable(double value) :
+                base(value)
+            {
+            }
+        }
+
+        class FakeUnit : UnitBase
+        {
+            public FakeUnit(string name, string symbol) :
+                base(name, symbol)
+            {
+            }
+        }
+
+        private FakeUnit meter = new FakeUnit("meter", "m");
+        private FakeUnit kilogram = new FakeUnit("kilogram", "kg");
+
+        [TestMethod]
+        public void ConstructorUnitValue()
+        {
+            FakeMeasurable m = new FakeMeasurable(meter, 1.0);
+
+            Assert.AreEqual<IUnit>(meter, m.Unit);
+            Assert.IsTrue(Math.Abs(m.Value - 1.0) < double.Epsilon);
+        }
+
+        [TestMethod]
+        public void ConstructorOnlyValue()
+        {
+            FakeMeasurable m = new FakeMeasurable(1.0);
+
+            Assert.AreEqual<IUnit>(ArbitraryUnit.Instance, m.Unit);
+            Assert.IsTrue(Math.Abs(m.Value - 1.0) < double.Epsilon);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorNullUnit()
+        {
+            FakeMeasurable m = new FakeMeasurable(null, 1.0);
+        }
+
+        [TestMethod]
+        public void CompareToSameUnits()
+        {
+            FakeMeasurable m1 = new FakeMeasurable(meter, 1.0);
+            FakeMeasurable m2 = new FakeMeasurable(meter, 2.0);
+
+            int compResult = m1.CompareTo(m2);
+            Assert.IsTrue(compResult < 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void CompareToDifferentUnits()
+        {
+            FakeMeasurable m1 = new FakeMeasurable(meter, 1.0);
+            FakeMeasurable m2 = new FakeMeasurable(kilogram, 2.0);
+
+            int compResult = m1.CompareTo(m2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CompareToNull()
+        {
+            FakeMeasurable m = new FakeMeasurable(meter, 1.0);
+            int compResult = m.CompareTo(null);
+        }
+
+        [TestMethod]
+        public void CompareToSameObjects()
+        {
+            FakeMeasurable m1 = new FakeMeasurable(meter, 1.0);
+            FakeMeasurable m2 = m1;
+
+            int compResult = m1.CompareTo(m2);
+            Assert.IsTrue(compResult == 0);
+        }
+
+        [TestMethod]
+        public void EqualsTypedNull()
+        {
+            FakeMeasurable m = new FakeMeasurable(meter, 1.0);
+
+            bool compResult = m.Equals(null);
+            Assert.IsFalse(compResult);
+        }
+
+        [TestMethod]
+        public void EqualsTypedSameObjects()
+        {
+            FakeMeasurable m1 = new FakeMeasurable(meter, 1.0);
+            FakeMeasurable m2 = m1;
+
+            bool compResult = m1.Equals(m2);
+            Assert.IsTrue(compResult);
+        }
+
+        [TestMethod]
+        public void EqualsTypedSameUnitsSameValues()
+        {
+            FakeMeasurable m1 = new FakeMeasurable(meter, 1.0);
+            FakeMeasurable m2 = new FakeMeasurable(meter, 1.0);
+
+            bool compResult = m1.Equals(m2);
+            Assert.IsTrue(compResult);
+        }
+
+        [TestMethod]
+        public void EqualsTypedSameUnitsDifferentValues()
+        {
+            FakeMeasurable m1 = new FakeMeasurable(meter, 1.0);
+            FakeMeasurable m2 = new FakeMeasurable(meter, 2.0);
+
+            bool compResult = m1.Equals(m2);
+            Assert.IsFalse(compResult);
+        }
+
+        [TestMethod]
+        public void EqualsTypedDifferentUnitsSameValues()
+        {
+            FakeMeasurable m1 = new FakeMeasurable(meter, 1.0);
+            FakeMeasurable m2 = new FakeMeasurable(kilogram, 1.0);
+
+            bool compResult = m1.Equals(m2);
+            Assert.IsFalse(compResult);
+        }
+
+        [TestMethod]
+        public void EqualsTypedDifferentUnitsDifferentValues()
+        {
+            FakeMeasurable m1 = new FakeMeasurable(meter, 1.0);
+            FakeMeasurable m2 = new FakeMeasurable(kilogram, 2.0);
+
+            bool compResult = m1.Equals(m2);
+            Assert.IsFalse(compResult);
+        }
+
+        [TestMethod]
+        public void ExplicitCastValid()
+        {
+            FakeMeasurable m = new FakeMeasurable(meter, 1.0);
+            double mValue = (double)m;
+
+            Assert.IsTrue(Math.Abs(m.Value - mValue) < double.Epsilon);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidCastException))]
+        public void ExplicitCastNull()
+        {
+            FakeMeasurable m = null;
+            double mValue = (double)m;
+        }
+    }
+}
--- opt.Domain/Units/MeasurableBase.cs
+++ opt.Domain/Units/MeasurableBase.cs
@@ -1,18 +1,32 @@
 ﻿using System;
 
-// TODO: Add unit tests
-// TODO: Add Equals(MeasurableBase<TValue>, IUnitConverter<TValue>) method? Or IUnitConversionProvider<TValue>? Or predicate?
-// TODO: Add CompareTo(MeasurableBase<TValue>, IUnitConverter<TValue>) method? Or IUnitConversionProvider<TValue>? Or predicate?
-
 namespace opt.Units
 {
+    /// <summary>
+    /// Class that can be used as a base for entity representing measurable value
+    /// (i.e. quantity measured in some units)
+    /// </summary>
+    /// <typeparam name="TValue">Type used to represent quantity (e.g. <see cref="Double"/>)</typeparam>
     public abstract class MeasurableBase<TValue> : IMeasurable<TValue>
         where TValue : struct, IComparable<TValue>, IEquatable<TValue>
     {
+        /// <summary>
+        /// Gets unit of measurement
+        /// </summary>
         public IUnit Unit { get; private set; }
 
+        /// <summary>
+        /// Gets or sets quantity
+        /// </summary>
         public TValue Value { get; set; }
 
+        /// <summary>
+        /// Initializes new instance of <see cref="MeasurableBase<TValue>"/> with
+        /// unit of measurement and quantity
+        /// </summary>
+        /// <param name="unit">Unit of measurement for this instance</param>
+        /// <param name="value">Quantity</param>
+        /// <exception cref="ArgumentNullException">If <paramref name="unit"/> is null</exception>
         protected MeasurableBase(IUnit unit, TValue value)
         {
             if (unit == null)
@@ -24,6 +38,11 @@ namespace opt.Units
             Value = value;
         }
 
+        /// <summary>
+        /// Initializes new instance of <see cref="MeasurableBase<TValue>"/> with
+        /// quantity and <see cref="ArbitraryUnit"/>
+        /// </summary>
+        /// <param name="value">Quantity</param>
         protected MeasurableBase(TValue value) :
             this(ArbitraryUnit.Instance, value)
         {
@@ -31,11 +50,27 @@ namespace opt.Units
 
         #region IComparable<IMeasurable<TValue>>
 
+        /// <summary>
+        /// Compares the current measurable with another measurable of the same type
+        /// </summary>
+        /// <param name="other">A measurable to compare with this one</param>
+        /// <returns>A value that indicates the relative order of the objects being compared.
+        /// The return value has the following meanings: Value Meaning Less than zero
+        /// This object is less than the other parameter.Zero This object is equal to
+        /// other. Greater than zero This object is greater than other</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="other"/> is null</exception>
+        /// <exception cref="InvalidOperationException">If units of measurement of this measurable
+        /// and <paramref name="other"/> measurable do not match</exception>
         public virtual Int32 CompareTo(IMeasurable<TValue> other)
         {
             if (object.ReferenceEquals((object)other, (object)null))
             {
                 throw new ArgumentNullException("other");
+            }
+
+            if (object.ReferenceEquals((object)other, (object)this))
+            {
+                return 0;
             }
 
             if (!Unit.Equals(other.Unit))
@@ -51,11 +86,23 @@ namespace opt.Units
 
         #region IEquatable<IMeasurable<TValue>>
 
+        /// <summary>
+        /// Indicates whether the current measurable is equal to another measurable of the same
+        /// type
+        /// </summary>
+        /// <param name="other">A measurable to compare with this one</param>
+        /// <returns>True if the current measurable is equal to the <paramref name="other"/>
+        /// (i.e. units of measurement are the same and values are equal); otherwise, false</returns>
         public virtual Boolean Equals(IMeasurable<TValue> other)
         {
             if (object.ReferenceEquals((object)other, (object)null))
             {
                 return false;
+            }
+
+            if (object.ReferenceEquals((object)other, (object)this))
+            {
+                return true;
             }
 
             return Unit.Equals(other.Unit) && Value.Equals(other.Value);
@@ -65,6 +112,14 @@ namespace opt.Units
 
         #region Operators
 
+        /// <summary>
+        /// Checks whether <paramref name="left"/> and <paramref name="right"/> measurables
+        /// are equal
+        /// </summary>
+        /// <param name="left">A measurable to compare to <paramref name="right"/></param>
+        /// <param name="right">A measurable to compare to <paramref name="left"/></param>
+        /// <returns>True if the current measurable is equal to the <paramref name="other"/>
+        /// (i.e. units of measurement are the same and values are equal); otherwise, false</returns>
         public static Boolean operator ==(MeasurableBase<TValue> left, MeasurableBase<TValue> right)
         {
             if (object.ReferenceEquals((object)left, (object)null))
@@ -75,6 +130,14 @@ namespace opt.Units
             return left.Equals((IMeasurable<TValue>)right);
         }
 
+        /// <summary>
+        /// Checks whether <paramref name="left"/> and <paramref name="right"/> measurables
+        /// are not equal
+        /// </summary>
+        /// <param name="left">A measurable to compare to <paramref name="right"/></param>
+        /// <param name="right">A measurable to compare to <paramref name="left"/></param>
+        /// <returns>True if the current measurable is not equal to the <paramref name="other"/>
+        /// (i.e. units of measurement are different or values are not equal); otherwise, false</returns>
         public static Boolean operator !=(MeasurableBase<TValue> left, MeasurableBase<TValue> right)
         {
             if (object.ReferenceEquals((object)left, (object)null))
@@ -85,6 +148,15 @@ namespace opt.Units
             return !left.Equals((IMeasurable<TValue>)right);
         }
 
+        /// <summary>
+        /// Checks whether <paramref name="left"/> is less than <paramref name="right"/>
+        /// </summary>
+        /// <param name="left">A measurable to compare to <paramref name="right"/></param>
+        /// <param name="right">A measurable to compare to <paramref name="left"/></param>
+        /// <returns>True if <paramref name="left"/> is less than <paramref name="right"/>;
+        /// otherwise, false</returns>
+        /// <exception cref="InvalidOperationException">If units of measurement of this measurable
+        /// and <paramref name="other"/> measurable do not match</exception>
         public static Boolean operator >(MeasurableBase<TValue> left, MeasurableBase<TValue> right)
         {
             if (object.ReferenceEquals((object)left, (object)null) ||
@@ -96,6 +168,15 @@ namespace opt.Units
             return left.CompareTo(right) > 0;
         }
 
+        /// <summary>
+        /// Checks whether <paramref name="left"/> is greater than <paramref name="right"/>
+        /// </summary>
+        /// <param name="left">A measurable to compare to <paramref name="right"/></param>
+        /// <param name="right">A measurable to compare to <paramref name="left"/></param>
+        /// <returns>True if <paramref name="left"/> is greater than <paramref name="right"/>;
+        /// otherwise, false</returns>
+        /// <exception cref="InvalidOperationException">If units of measurement of this measurable
+        /// and <paramref name="other"/> measurable do not match</exception>
         public static Boolean operator <(MeasurableBase<TValue> left, MeasurableBase<TValue> right)
         {
             if (object.ReferenceEquals((object)left, (object)null) ||
@@ -111,11 +192,21 @@ namespace opt.Units
 
         #region Object overrides
 
+        /// <summary>
+        /// Determines whether the specified <see cref="Object"/> is equal to the current <see cref="Object"/>
+        /// </summary>
+        /// <param name="obj">The <see cref="Object"/> to compare with the current <see cref="Object"/></param>
+        /// <returns>True if the specified <see cref="Object"/> is equal to the current <see cref="Object"/>;
+        ///     otherwise, false</returns>
         public override Boolean Equals(object obj)
         {
             return Equals(obj as IMeasurable<TValue>);
         }
 
+        /// <summary>
+        /// Serves as a hash function for <see cref="MeasurableBase<TValue>"/>
+        /// </summary>
+        /// <returns>A hash code for the current <see cref="MeasurableBase<TValue>"/></returns>
         public override Int32 GetHashCode()
         {
             // http://stackoverflow.com/questions/263400/what-is-the-best-algorithm-for-an-overridden-system-object-gethashcode
@@ -133,6 +224,12 @@ namespace opt.Units
 
         #region Explicit cast to TValue
 
+        /// <summary>
+        /// Converts <see cref="MeasurableBase<TValue>"/> to <typeparamref name="TValue"/>
+        /// </summary>
+        /// <param name="measurable"><see cref="MeasurableBase<TValue>"/> to be converted
+        /// to <typeparamref name="TValue"/></param>
+        /// <returns><typeparamref name="TValue"/></returns>
         public static explicit operator TValue(MeasurableBase<TValue> measurable)
         {
             if (measurable == null)
@@ -143,6 +240,10 @@ namespace opt.Units
             return measurable.Value;
         }
 
+        /// <summary>
+        /// Converts current <see cref="MeasurableBase<TValue>"/> to <typeparamref name="TValue"/>
+        /// </summary>
+        /// <returns><typeparamref name="TValue"/></returns>
         public virtual TValue ToValueType()
         {
             return (TValue)this;
